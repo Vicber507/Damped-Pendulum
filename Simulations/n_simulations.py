19f17fed6fc9e586d2eadd1f5b0631b35be581eb
Vicber507 <victorbernalcano507@gmail.com>
2024-11-19 22:07:55 -0500

    more vids

--- Simulations/n_simulations.py
+++ Simulations/n_simulations.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 # Constantes
-g = 9.81  # Aceleración debido a la gravedad (m/s^2)
+g = 9.82  # Aceleración debido a la gravedad (m/s^2)
 L = 0.57  # Longitud del péndulo (m)
 m = 0.22610  # Masa del péndulo (kg)
-gamma = 0.620
+gamma = 10
 
 def pendulum_derivatives(y, t, g, L, gamma, m, n):
     theta, omega = y
     dtheta_dt = omega
-    domega_dt = -np.sign(omega) * (gamma) * np.abs(omega)**n - (g / L) * np.sin(theta)
+    domega_dt = - np.sign(omega) * (gamma) * np.abs(omega)**n - ((g / L) * np.sin(theta))
     return [dtheta_dt, domega_dt]
 
 # Tiempo de simulación
-t = np.linspace(0, 30, 1000)
+t = np.linspace(0, 15, 1000)
 
 plt.figure(figsize=(8, 6))
 
 # Simulación y gráfico
-y0 = [0.15, 0]  # Condiciones iniciales
-colors = plt.cm.viridis(np.linspace(0, 1, 3))  # Colormap para 10 colores
+y0 = [0.10, 0]  # Condiciones iniciales
+colors = plt.cm.viridis(np.linspace(0, 1, 5))  
 
-for i in range(1, 4):
-    sol = odeint(pendulum_derivatives, y0, t, args=(g, L, gamma, m, i))  # Solución de la ODE
+for i in range(1, 6):
+    sol = odeint(pendulum_derivatives, y0, t, args=(g, L, gamma, m, i))  
     theta = sol[:, 0]  # Ángulo en el tiempo
     plt.plot(t, theta, color=colors[i-1], linestyle='--', linewidth=1.5, markersize=3, label=f'n = {i}')
 
